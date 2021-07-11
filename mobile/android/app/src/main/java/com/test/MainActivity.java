62c77c028bf5fee41a82cadf36485ef17c09cb06
lke1221 <lke001221@gmail.com>
2021-07-11 14:58:22 +0900

    0711

---
+++ mobile/android/app/src/main/java/com/test/MainActivity.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "test";
+  }
+}
